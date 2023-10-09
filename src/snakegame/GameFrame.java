31b898d7180311652ec3e8250fb915173f18a93f
gs-gokul <gsmusix@gmail.com>
2023-10-09 17:36:15 +0530

    first commit

---
+++ src/snakegame/GameFrame.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snakegame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author GK MUSIX
+ */
+public class GameFrame extends JFrame implements ActionListener {
+    
+    GamePanel game;
+    JButton resetButton;
+    
+    GameFrame(){
+        
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        }
+        
+    }
+    
+
